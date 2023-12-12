17175aaed4d7ca0328745a32c28bdcb8d1f11e35
Prasad Lohakare <lohakare3@gmail.com>
2023-12-12 19:26:37 +0530

    Line No 5 added

--- Calculator/src/Functions/Add.java
+++ Calculator/src/Functions/Add.java
@@ -2,10 +2,12 @@ package Functions;
 
 public class Add {
 	public static void main(String[] args) {
+		System.out.println("Addition Program");
 		int a=10;
 		int b=10;
 		int c=a+b;
 		System.out.println("Addition is : "+c);
+		
 	}
 
 }
