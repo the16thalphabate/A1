afb7df302458d688a5f88c7c3ef1781e6f4730a6
Prasad Lohakare <lohakare3@gmail.com>
2023-12-12 19:20:45 +0530

    Addition  modified in line no 7

--- Calculator/src/Functions/Add.java
+++ Calculator/src/Functions/Add.java
@@ -4,7 +4,8 @@ public class Add {
 	public static void main(String[] args) {
 		int a=10;
 		int b=10;
-		System.out.println("Addition "+a+b);
+		int c=a+b;
+		System.out.println("Addition "+c);
 	}
 
 }
